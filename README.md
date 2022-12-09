63b8dd1b276b85d77f5c8b8ec65d0fcff9c31a00
Junjie Yang <yangjunjie0320@gmail.com>
2022-12-09 15:21:13 -0800

    make it work

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # pydmet
+- Fragmet is a set of LO indices, which contains two parts, one is the impurity part (defined by the user), 
+  the other is the environment part (obtained by the `build_env` function, which will exclude the virtual
+  and core orbitals). The `build_emb_basis` function will stack the impurity orbitals and construct the 
+  bath orbitals from the environment part.
+- Fragment is also used to refer to the orbitals in the embedding space, which contains the impurity EOs
+  (shall be the same as impurity LOs), and the bath EOs (constructed from the environment LOs).
+- solver: the solver attribute could be a list of solvers or a childclass of `SolverMixin` (see `solver.py`).
+  The solver is used to solve the impurity problem. The `DMET` class will call the `solve` function of the 
+  solver to solve the impurity problem. The `DMET` class will also call the `update` function of the solver 
+  to update the solver after the impurity problem is solved.
+- The DMET is classified as the scf classes in pyscf, the mean filed object will be created and maintained 
+  inside the DMET object. Using KS DFT as the mean field is not recommended, as the energy formula might
+  not be correct.
+- If the impurities are choosen to be a partition of the whole system, the rest part of the orbitals could
+  also form a fragment with HF solver, (seems to be a trivial case, not sure if is the case for the k-point 
+  version), while the rest part corresponds to a small bath, with the naturally converged HF solution from
+  DMET object, no integral transformation is needed, just transorm the density matrix to this embedding 
+  basis.
--- pydmet/dmet/rdmet.py
+++ pydmet/dmet/rdmet.py
@@ -17,44 +17,75 @@ class EmbeddingProblem(object):
 class EmbeddingResults(object):
     pass
 
+class EmbeddingBasis(object):
+    pass
+
 class SolverMixin(object):
+    verbose  = 5
+    stdout   = sys.stdout
     tmp_dir  = None
 
     conv_tol = 1e-8
     conv_tol_grad = 1e-5
     max_cycle = 50
 
+    def dump_res(self):
+        raise NotImplementedError
+    
+    def load_res(self):
+        raise NotImplementedError
 
     def kernel(self, emb_prob, load_dir=None, save_dir=None):
         raise NotImplementedError
 
-class RHF(SolverMixin):
-    def kernel(self, emb_prob, load_dir=None, save_dir=None):
-        norb   = emb_prob.norb
+    def solve_mf(self, emb_prob, load_dir=None, save_dir=None):
+        norb   = emb_prob.neo
         nelecs = emb_prob.nelecs
         neleca, nelecb = nelecs
         assert neleca == nelecb
+        dm0 = emb_prob.dm0
 
         m = gto.M()
-        m.nelectron     = nelecs
+        m.nelectron     = neleca + nelecb
+        m.spin          = 0
         m.incore_anyway = True
+        m.build()
 
         s1e = numpy.eye(norb)
-        h1e = emb_prob.h1e
+        f1e = emb_prob.f1e
         h2e = emb_prob.h2e
 
         h = pyscf.scf.RHF(m)
+        h.verbose   = self.verbose
+        h.stdout    = self.stdout
         h.get_ovlp  = lambda *args: s1e
-        h.get_hcore = lambda *args: h1e
+        h.get_hcore = lambda *args: f1e
         h._eri      = h2e
         h.conv_tol  = self.conv_tol
         h.max_cycle = self.max_cycle
-        h.kernel()
+        h.kernel(dm0=dm0)
 
         if h.converged:
             logger.info(self, 'RHF converged')
         else:
             logger.info(self, 'RHF not converged')
+        
+        return h
+
+    def energy_elec(self, rdm1, rdm2, emb_prob=None):
+        imp_eo_idx  = emb_prob.imp_eo_idx
+        f1e = emb_prob.f1e
+        h1e = emb_prob.h1e
+        h2e = emb_prob.h2e
+        
+        ene_elec    = numpy.einsum('pq,  qp  ->', (f1e + h1e)[imp_eo_idx, :], rdm1[:, imp_eo_idx])       / 2.0
+        ene_elec   += numpy.einsum('pqrs,pqrs->', h2e[imp_eo_idx, :, :, :],   rdm2[imp_eo_idx, :, :, :]) / 2.0
+        return ene_elec
+
+class RHF(SolverMixin):
+
+    def kernel(self, emb_prob, load_dir=None, save_dir=None):
+        h = self.solve_mf(emb_prob, load_dir, save_dir)
 
         mo_coeff = h.mo_coeff
         rdm1 = h.make_rdm1()
@@ -73,14 +104,16 @@ class RHF(SolverMixin):
             chkfile.save(save_file, 'scf', scf_res)
 
         emb_res = EmbeddingResults()
-        emb_res.rdm1 = rdm1
-        emb_res.rdm2 = rdm2
-        emb_res.mo_coeff = mo_coeff
+        emb_res.neo    = emb_prob.neo
+        emb_res.nelecs = emb_prob.nelecs
+        emb_res.rdm1    = rdm1
+        emb_res.rdm1_ao = None
+        emb_res.energy_elec = self.energy_elec(rdm1, rdm2, emb_prob)
         return emb_res
 
 class RCCSD(SolverMixin):
     def kernel(self, emb_prob, load_dir, save_dir):
-        norb   = emb_prob.norb
+        norb   = emb_prob.neo
         nelecs = emb_prob.nelecs
         neleca, nelecb = nelecs
         assert neleca == nelecb
@@ -127,8 +160,8 @@ class RCCSD(SolverMixin):
         cc.max_cycle = self.max_cycle
         cc.kernel()
 
-        rdm1 = cc.make_rdm1()
-        rdm2 = cc.make_rdm2()
+        rdm1 = cc.make_rdm1(ao_repr=True)
+        rdm2 = cc.make_rdm2(ao_repr=True)
 
         if cc.converged:
             logger.info(self, 'RCCSD converged')
@@ -164,6 +197,9 @@ class DMETMixin(object):
     conv_tol  = 1e-6
     max_cycle = 50
 
+    save_dir = None
+    load_dir = None
+
     def _method_name(self):
         method_name = []
         for c in self.__class__.__mro__:
@@ -178,13 +214,13 @@ class DMETMixin(object):
         log.info("******** %s ********", self.__class__)
         log.info('method = %s', self._method_name())
 
+    def build_env(self):
+        raise NotImplementedError
+
     def build_mf(self):
         raise NotImplementedError
 
     def build_emb_basis(self):
-        """
-        Build the embedding basis for the impurity.
-        """
         raise NotImplementedError
 
     def build_emb_prob(self):
@@ -195,6 +231,9 @@ class DMETMixin(object):
         assert solver is not None
 
         if isinstance(solver, Iterable):
+            solver_list = list(solver)
+            assert len(solver_list) == self.nfrag
+
             solver_obj = solver[ifrag]
         else:
             solver_obj = solver
@@ -217,24 +256,37 @@ class DMETMixin(object):
     def kernel(self, dm0=None):
         self.dump_flags()
         log = logger.new_logger(self, self.verbose)
-        
+
         m   = self.m
         mf  = self.build_mf(m, dm0=dm0)
         assert mf.converged
         coeff_ao_lo = self.coeff_ao_lo
 
+        hcore_ao = mf.get_hcore()
+        ovlp_ao  = mf.get_ovlp()
+        eri_ao   = None
+        if hasattr(mf, '_eri'):
+            eri_ao = mf._eri
+
+        self.build_env()
         nfrag = self.nfrag
         imp_lo_idx_list = self.imp_lo_idx_list
         env_lo_idx_list = self.env_lo_idx_list
+        assert len(imp_lo_idx_list) == nfrag
+        assert len(env_lo_idx_list) == nfrag
 
         dm_ll_ao_pre = mf.make_rdm1()
-        dm_ll_lo_pre = self.transform_dm_ao_to_lo(dm_ll_ao_pre, coeff_ao_lo=coeff_ao_lo, mf=mf)
+        dm_ll_lo_pre = self.transform_dm_ao_to_lo(dm_ll_ao_pre, coeff_ao_lo=coeff_ao_lo, ovlp_ao=ovlp_ao)
         dm_ll_ao_cur = None
         dm_ll_lo_cur = None
-        
-        emb_res_list = []
 
-        for ifrag, imp_idx, env_idx in zip(range(nfrag), imp_lo_idx_list, env_lo_idx_list):
+        ene_hl_cur = 0.0
+        ene_hl_pre = None
+
+        dm_hl_ao_cur = numpy.zeros_like(dm_ll_ao_pre)
+        dm_hl_lo_cur = None
+
+        for ifrag, imp_lo_idx, env_lo_idx in zip(range(nfrag), imp_lo_idx_list, env_lo_idx_list):
             save_dir = self.save_dir 
             if save_dir is not None:
                 save_dir = os.path.join(save_dir, 'frag_%d' % ifrag)
@@ -243,30 +295,40 @@ class DMETMixin(object):
 
             load_dir = None
 
-            nlo_imp = len(imp_idx)
-            nlo_env = len(env_idx)
-
-            res         = self.build_emb_basis(imp_idx, env_idx, dm_ll_lo_pre)
-            coeff_ao_eo, coeff_lo_eo= res
+            emb_basis = self.build_emb_basis(
+                imp_lo_idx, env_lo_idx, dm_ll_lo_pre,
+                coeff_ao_lo=coeff_ao_lo, ovlp_ao=ovlp_ao
+                )
 
             emb_solver = self.get_emb_solver(ifrag)
             emb_prob   = self.build_emb_prob(
-                m, mf, coeff_lo_eo, coeff_ao_eo,
+                m, mf, emb_basis=emb_basis,
+                hcore_ao=hcore_ao, eri_ao=eri_ao,
                 dm_ll_lo=dm_ll_lo_pre, 
-                dm_ll_ao=dm_ll_ao_pre,
+                dm_ll_ao=dm_ll_ao_pre
                 )
 
-            emb_res    = emb_solver.kernel(
-                emb_prob, save_dir=save_dir, load_dir=load_dir,
+            # Note that the embbedding results from the solver may
+            # not be consistent with the DMET type. So we need to
+            # transform the results to the DMET type.
+            emb_res = emb_solver.kernel(
+                emb_prob=emb_prob,
+                save_dir=save_dir, 
+                load_dir=load_dir,
             )
-            emb_res_list.append(emb_res)
+
+            ene_hl_cur   += emb_res.energy_elec
+            dm_hl_ao_cur += self.get_rdm1_ao(emb_res, emb_basis)
+
+        dm_hl_lo_cur = self.transform_dm_ao_to_lo(dm_hl_ao_cur, coeff_ao_lo=coeff_ao_lo, ovlp_ao=ovlp_ao)
+        print("ene_hl_cur = %8.6f"%ene_hl_cur)
 
 class RDMETWithHF(DMETMixin):
     '''The class for solving spin restricted DMET problem in molecular system
     and the supercell gamma point periodic system.
     '''
 
-    def __init__(self, mol, coeff_ao_lo=None, imp_lo_list=None, solver=None):
+    def __init__(self, mol, coeff_ao_lo=None, imp_lo_idx_list=None, solver=None):
         self.m       = mol
         self.stdout  = mol.stdout
         self.verbose = mol.verbose
@@ -274,17 +336,22 @@ class RDMETWithHF(DMETMixin):
         assert coeff_ao_lo is not None
         self.coeff_ao_lo = coeff_ao_lo
 
-        assert imp_lo_list is not None
-        self.imp_lo_list = imp_lo_list
+        assert imp_lo_idx_list is not None
+        self.imp_lo_idx_list = imp_lo_idx_list
+        self.nfrag           = len(imp_lo_idx_list)
+        self.env_lo_idx_list = None
 
         if solver is None:
             solver = RHF()
         else:
             if isinstance(solver, Iterable):
+                solver = list(solver)
                 for s in solver:
                     assert isinstance(s, SolverMixin)
+                assert len(solver) == self.nfrag
             else:
                 assert isinstance(solver, SolverMixin)
+        self.solver = solver
 
     def transform_dm_ao_to_lo(self, dm_ao, ovlp_ao=None, coeff_ao_lo=None):
         dm_lo    = reduce(numpy.dot, (coeff_ao_lo.T, ovlp_ao, dm_ao, ovlp_ao, coeff_ao_lo))
@@ -302,6 +369,13 @@ class RDMETWithHF(DMETMixin):
         h_ao    = reduce(numpy.dot, (coeff_ao_lo.T, ovlp_ao, h_lo, ovlp_ao, coeff_ao_lo))
         return h_ao
 
+    def build_env(self):
+        if self.env_lo_idx_list is None:
+            self.env_lo_idx_list = []
+            for imp_lo_idx in self.imp_lo_idx_list:
+                env_lo_idx = [i for i in range(self.m.nao) if i not in imp_lo_idx]
+                self.env_lo_idx_list.append(env_lo_idx)
+
     def build_mf(self, m, dm0=None):
         log = logger.new_logger(self, self.verbose)
         assert isinstance(m, pyscf.gto.Mole)
@@ -317,7 +391,47 @@ class RDMETWithHF(DMETMixin):
 
         return mf
 
+    def get_rdm1_ao(self, emb_res, emb_basis):
+        neo  = emb_res.neo
+
+        imp_eo_idx  = emb_basis.imp_eo_idx
+        bath_eo_idx = emb_basis.bath_eo_idx
+        coeff_ao_eo = emb_basis.coeff_ao_eo
+        coeff_ao_eo_imp  = coeff_ao_eo[:, imp_eo_idx]
+        coeff_ao_eo_bath = coeff_ao_eo[:, bath_eo_idx]
+
+        rdm1_emb_eo = emb_res.rdm1
+        rdm1_emb_eo_imp_imp   = rdm1_emb_eo[imp_eo_idx, :][:, imp_eo_idx]
+        rdm1_emb_eo_imp_bath  = rdm1_emb_eo[imp_eo_idx, :][:, bath_eo_idx]
+        rdm1_emb_eo_bath_imp  = rdm1_emb_eo[bath_eo_idx, :][:, imp_eo_idx]
+
+        rdm1_ao  = reduce(numpy.dot, (coeff_ao_eo_imp, rdm1_emb_eo_imp_imp,  coeff_ao_eo_imp.T))
+        rdm1_ao += reduce(numpy.dot, (coeff_ao_eo_imp, rdm1_emb_eo_imp_bath, coeff_ao_eo_bath.T)) * 0.5
+        rdm1_ao += reduce(numpy.dot, (coeff_ao_eo_bath, rdm1_emb_eo_bath_imp, coeff_ao_eo_imp.T)) * 0.5
+
+        return rdm1_ao
+
     def build_emb_basis(self, imp_lo_idx, env_lo_idx, dm_ll_lo, ovlp_ao=None, coeff_ao_lo=None):
+        """Build the embedding basis for the given impurity and environment orbitals.
+        
+        Parameters:
+            imp_lo_idx: list of int
+                The indices of the impurity orbitals.
+            env_lo_idx: list of int
+                The indices of the environment orbitals.
+            dm_ll_lo: 2D numpy array
+                The density matrix in the local basis, from the low level (mean field) calculation.
+            ovlp_ao: 2D numpy array
+                The overlap matrix in the AO basis.
+            coeff_ao_lo: 2D numpy array
+                The transformation matrix from the AO basis to the local basis.
+            
+        Returns:
+            coeff_ao_eo: 2D numpy array
+                The transformation matrix from the AO basis to the embedding basis.
+            coeff_lo_eo: 2D numpy array
+                The transformation matrix from the local basis to the embedding basis.
+        """
         nlo_imp = len(imp_lo_idx)
         nlo_env = len(env_lo_idx)
 
@@ -336,22 +450,40 @@ class RDMETWithHF(DMETMixin):
         assert dm_imp_env_lo.shape == (nlo_imp, nlo_env)
 
         u, s, vh = numpy.linalg.svd(dm_imp_env_lo, full_matrices=False)
-        coeff_lo_eo_imp = numpy.eye(nlo_imp)
-        coeff_lo_eo_env = vh.T
+        coeff_lo_eo_imp_imp  = numpy.eye(nlo_imp)
+        coeff_lo_eo_env_bath = vh.T
 
-        coeff_ao_eo_imp = numpy.dot(coeff_ao_lo[:, imp_lo_idx], coeff_lo_eo_imp)
-        coeff_ao_eo_env = numpy.dot(coeff_ao_lo[:, env_lo_idx], coeff_lo_eo_env)
-        coeff_ao_eo = numpy.hstack((coeff_ao_eo_imp, coeff_ao_eo_env))
+        # The embedding basis is the union of the impurity and environment basis.
+        coeff_ao_lo_imp = coeff_ao_lo[:, imp_lo_idx]
+        coeff_ao_lo_env = coeff_ao_lo[:, env_lo_idx]
+        coeff_ao_eo_imp  = numpy.dot(coeff_ao_lo_imp, coeff_lo_eo_imp_imp)
+        coeff_ao_eo_bath = numpy.dot(coeff_ao_lo_env, coeff_lo_eo_env_bath)
+        coeff_ao_eo = numpy.hstack((coeff_ao_eo_imp, coeff_ao_eo_bath))
         coeff_lo_eo = reduce(numpy.dot, (coeff_ao_lo.T, ovlp_ao, coeff_ao_eo))
 
         neo = coeff_ao_eo.shape[1]
+        nlo_imp  = coeff_ao_eo_imp.shape[1]
+        nlo_env  = coeff_ao_lo_env.shape[1]
+        neo_imp  = coeff_ao_eo_imp.shape[1]
+        neo_bath = coeff_ao_eo_bath.shape[1]
         assert coeff_ao_eo.shape == (nao, neo)
         assert coeff_lo_eo.shape == (nlo, neo)
 
-        return coeff_ao_eo, coeff_lo_eo
+        emb_basis = EmbeddingBasis()
+        emb_basis.nlo = nlo
+        emb_basis.nao = nao
+        emb_basis.neo = neo
+        emb_basis.neo_imp = neo_imp
+        emb_basis.neo_bath = neo_bath
+        emb_basis.imp_eo_idx  = range(0, neo_imp)
+        emb_basis.bath_eo_idx = range(neo_imp, neo)
+        emb_basis.coeff_ao_eo = coeff_ao_eo
+        emb_basis.coeff_lo_eo = coeff_lo_eo
 
-    def build_emb_prob(self, mf, coeff_lo_eo, coeff_ao_eo,
-                       hcore_ao=None, eri_ao=None,
+        return emb_basis
+
+    def build_emb_prob(self, m, mf,   emb_basis=None,
+                       hcore_ao=None, eri_ao=None, 
                        dm_ll_lo=None, dm_ll_ao=None):
         """Build the embedding problem for the given impurity and environment orbitals.
         
@@ -380,12 +512,14 @@ class RDMETWithHF(DMETMixin):
         assert dm_ll_lo is not None
         assert dm_ll_ao is not None
 
-        nao, neo = coeff_ao_eo.shape
-        nlo = coeff_lo_eo.shape[0]
-        assert coeff_ao_eo.shape == (nao, neo)
-        assert coeff_lo_eo.shape == (nlo, neo)
-        assert dm_ll_ao.shape    == (nao, nao)
-        assert dm_ll_lo.shape    == (nlo, nlo)
+        neo = emb_basis.neo
+        neo_imp = emb_basis.neo_imp
+        neo_bath = emb_basis.neo_bath
+        imp_eo_idx = emb_basis.imp_eo_idx
+        bath_eo_idx = emb_basis.bath_eo_idx
+
+        coeff_ao_eo = emb_basis.coeff_ao_eo
+        coeff_lo_eo = emb_basis.coeff_lo_eo
 
         # Get the valence part of the density matrix in LO basis
         dm_ll_eo     = reduce(numpy.dot, (coeff_lo_eo.T, dm_ll_lo, coeff_lo_eo))
@@ -400,27 +534,32 @@ class RDMETWithHF(DMETMixin):
         f1e_eo       = h1e_eo + veff_eo_cor
 
         eri_eo = None
-        if eri_ao is None:
-            assert isinstance(mf.mol, pyscf.gto.Mole)
-            eri_eo = pyscf.ao2mo.incore.full(eri_ao, coeff_ao_eo)
+        if isinstance(m, pyscf.gto.Mole) or isinstance(m, pyscf.gto.Mole):
+            eri_eo = pyscf.ao2mo.kernel(m, coeff_ao_eo, compact=False)
         else:
-            eri_eo = pyscf.ao2mo.incore.full(eri_ao, coeff_ao_eo)
+            assert eri_ao is not None
+            eri_eo = pyscf.ao2mo.kernel(eri_ao, coeff_ao_eo, compact=False)
         eri_eo_full = pyscf.ao2mo.restore(1, eri_eo, neo)
 
         nelec = numpy.einsum('ii->', dm_ll_eo)
         nelec = numpy.round(nelec)
         nelec = int(nelec)
+
         assert nelec % 2 == 0
         nelecs = (nelec // 2, nelec // 2)
         
         emb_prob = EmbeddingProblem()
-        emb_prob.nao   = nao
-        emb_prob.nlo   = nlo
-        emb_prob.neo   = neo
-        emb_prob.norb  = neo
+        emb_prob.nao    = nao
+        emb_prob.nlo    = nlo
+        emb_prob.neo    = neo
+        emb_prob.neo_imp  = neo_imp
+        emb_prob.neo_bath = neo_bath
+        emb_prob.imp_eo_idx = imp_eo_idx
+        emb_prob.bath_eo_idx = bath_eo_idx
         emb_prob.nelecs = nelecs
         emb_prob.dm0    = dm_ll_eo
-        emb_prob.h1e    = f1e_eo
+        emb_prob.h1e    = h1e_eo
+        emb_prob.f1e    = f1e_eo
         emb_prob.h2e    = eri_eo_full
 
         return emb_prob
@@ -447,7 +586,7 @@ if __name__ == '__main__':
            H         3.0247679096    0.0221180670    1.0833062723
         """,
         basis = 'sto3g',
-        verbose=4
+        verbose=0
     )
     ao_labels = mol.ao_labels(fmt=False)
 
@@ -487,5 +626,7 @@ if __name__ == '__main__':
         print("  AO indices: ", imp_ao_idx_list[ifrag])
         print("  LO indices: ", imp_lo_idx_list[ifrag])
 
-    dmet_obj = RDMETWithHF(mol, coeff_ao_lo=coeff_ao_lo, imp_lo_list=imp_lo_idx_list, solver=None)
+    dmet_obj = RDMETWithHF(mol, coeff_ao_lo=coeff_ao_lo, imp_lo_idx_list=imp_lo_idx_list, solver=None)
+    dmet_obj.verbose = 0
     dmet_obj.kernel(dm0 = mf.make_rdm1())
+    print("E = %.12f" % mf.energy_elec()[0])
